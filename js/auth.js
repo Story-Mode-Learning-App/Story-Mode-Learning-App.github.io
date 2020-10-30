70faff22cb198dfd04e85a93d1c8c53c8bea4e96
Maheyan <68837961+Maheyan@users.noreply.github.com>
2020-10-30 12:14:11 +0600

    new

--- js/auth.js
+++ js/auth.js
@@ -18,7 +18,16 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
         signInOptions: [
 
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          {
+            provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            recaptchaParameters: {
+              type: 'image',
+              size: 'invisible',
+              badge: 'bottomleft'
+            },
+      		whitelistedCountries: ['BD', '+880']
+          }
         ],
       }; 
 ui.start('#firebaseui-auth-container', uiConfig);
