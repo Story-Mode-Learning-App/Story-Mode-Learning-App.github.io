6f17bcbb284cf2b4d80e14eaf9e065c3bc240997
Maheyan <68837961+Maheyan@users.noreply.github.com>
2020-11-01 18:15:55 +0600

    new

---
+++ js/modal-home.js
@@ -0,0 +1,14 @@
+var modal = document.getElementsByClassName('modal');
+var btn = document.getElementsByClassName("myBtn");
+var span = document.getElementsByClassName("close");
+btn[0].onclick = function() {
+    modal[0].style.display = "block";
+}
+span[0].onclick = function() {
+    modal[0].style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
