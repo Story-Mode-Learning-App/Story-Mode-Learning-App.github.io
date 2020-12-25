db690c6a538fcf757e9f347448bf5428fce84ff0
Maheyan <68837961+Maheyan@users.noreply.github.com>
2020-12-25 19:42:39 +0600

    Update home.js

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ var mainApp = {};
 (function() {
     var firebase = app_firebase;
 var uid = null;
-$.get('https://json.geoiplookup.io/', function(res) {var a = ("Reg. IP : " + res.ip + "<br>" + "District : " + res.district + "<br>" + "Country : " + res.country_name);document.getElementById("ip-details").innerHTML = a;});
+$.get('https://json.geoiplookup.io/', function(res) {var c = ("Reg. IP : " + res.ip + "<br>" + "District : " + res.district + "<br>" + "Country : " + res.country_name);document.getElementById("ip-details").innerHTML = c;});
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     uid = user.uid;
