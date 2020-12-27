6a1fa2143d0840f862868584860120ee83b1ef30
Maheyan <68837961+Maheyan@users.noreply.github.com>
2020-12-28 03:19:14 +0600

    updated

--- js/home.js
+++ js/home.js
@@ -11,33 +11,33 @@ firebase.auth().onAuthStateChanged(function(user) {
     emailVerified = user.emailVerified;
     photoUrl = user.photoURL;
         document.getElementById("dp").innerHTML = '<img src="' + photoUrl +'" class="rounded mx-auto d-block" alt="dp">';
-        document.getElementById("userName").innerHTML = "Name : " + uname;
-        document.getElementById("userName2").innerHTML = "Phone : N/A";
-        document.getElementById("uemail").innerHTML = "Email : " + email;
-        document.getElementById("licenseFor").innerHTML = "License : " + uid;
+        document.getElementById("userName").innerHTML = "<strong>Name :</strong> " + uname;
+        document.getElementById("userName2").innerHTML = "<strong>Phone :</strong> N/A";
+        document.getElementById("uemail").innerHTML = "<strong>Email :</strong> " + email;
+        document.getElementById("licenseFor").innerHTML = "<strong>License :</strong> " + uid;
         if(emailVerified == true) {
-          document.getElementById("Subscription").innerHTML = "Subscription : Free Trial";
+          document.getElementById("Subscription").innerHTML = "<strong>Subscription :</strong> Free Trial";
         }
         if(email == null) {
-          document.getElementById("uemail").innerHTML = "Email : N/A";
+          document.getElementById("uemail").innerHTML = "<strong>Email :</strong> N/A";
         }
         if(emailVerified != true) {
-          document.getElementById("Subscription").innerHTML = "Subscription : Free Trial";
+          document.getElementById("Subscription").innerHTML = "<strong>Subscription :</strong> Free Trial";
         }
         if(uname == null) {
-          document.getElementById("userName").innerHTML = "Name : N/A";
-          document.getElementById("uemail").innerHTML = "Email : N/A";
+          document.getElementById("userName").innerHTML = "<strong>Name :</strong> N/A";
+          document.getElementById("uemail").innerHTML = "<strong>Email :</strong> N/A";
           document.getElementById("dp").innerHTML = "";
         }
 if (user != null) {
   user.providerData.forEach(function (profile) {
-    document.getElementById("secured").innerHTML = "Verified by : " + profile.providerId;
+    document.getElementById("secured").innerHTML = "<strong>Verified by :</strong> " + profile.providerId;
   });
 }
 if (user != null && uname == null) {
   user.providerData.forEach(function (profile) {
-    document.getElementById("secured").innerHTML = "Verified by : " + profile.providerId;
-    document.getElementById("userName2").innerHTML = "Phone : " + profile.uid;
+    document.getElementById("secured").innerHTML = "<strong>Verified by :</strong> " + profile.providerId;
+    document.getElementById("userName2").innerHTML = "<strong>Phone :</strong> " + profile.uid;
   });
 }
   }else {
