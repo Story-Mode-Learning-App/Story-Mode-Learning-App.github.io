5ae6afbba5e2b0c291ca9876724bc73fa8f24dea
ASG SHOP <maheyan6484@gmail.com>
2025-11-03 13:53:07 +0800

    final fix

--- js/story.js
+++ js/story.js
@@ -1,28 +1,34 @@
 var mainApp = {};
-(function() {
-    var firebase = app_firebase;
-var uid = null;
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    uid = user.uid;
-    var iframe = document.getElementsByClassName("carousel-control-next")[0]
-    if (document.fullscreenEnabled ||
-          document.webkitFullscreenEnabled ||
-          document.mozFullScreenEnabled ||
-          document.msFullscreenEnabled) {
-
-          if (iframe.requestFullscreen) {
-            iframe.requestFullscreen();
-          } else if (iframe.webkitRequestFullscreen) {
-            iframe.webkitRequestFullscreen();
-          } else if (iframe.mozRequestFullScreen) {
-            iframe.mozRequestFullScreen();
-          } else if (iframe.msRequestFullscreen) {
-            iframe.msRequestFullscreen();
-          }
-        }
-  }else {
+(function () {
+  var firebase = app_firebase;
+  var uid = null;
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      uid = user.uid;
+      var nextbutton = document.getElementsByClassName("carousel-control-next")[0]
+      nextbutton.addEventListener('click', fullscreen);
+    } else {
       window.location.replace("../../../../");
-  }
-});
+    }
+  });
 })()
+
+var iframe = document.getElementById("carouselFade");
+
+function fullscreen() {
+  if (document.fullscreenEnabled ||
+    document.webkitFullscreenEnabled ||
+    document.mozFullScreenEnabled ||
+    document.msFullscreenEnabled) {
+
+    if (iframe.requestFullscreen) {
+      iframe.requestFullscreen();
+    } else if (iframe.webkitRequestFullscreen) {
+      iframe.webkitRequestFullscreen();
+    } else if (iframe.mozRequestFullScreen) {
+      iframe.mozRequestFullScreen();
+    } else if (iframe.msRequestFullscreen) {
+      iframe.msRequestFullscreen();
+    }
+  }
+}
