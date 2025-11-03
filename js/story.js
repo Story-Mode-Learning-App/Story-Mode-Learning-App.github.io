d48d8e72f772d9ce556a1b0db965c052aeb18d7e
ASG SHOP <maheyan6484@gmail.com>
2025-11-03 14:33:59 +0800

    fixed all

--- js/story.js
+++ js/story.js
@@ -40,15 +40,3 @@ function exitfullscreen() {
       document.exitFullscreen()
     }
   }
-
-    $('#carouselFade').on('slid.bs.carousel', function () {
-    const $carousel = $(this);
-    const totalItems = $carousel.find('.carousel-item').length;
-    const currentIndex = $carousel.find('.carousel-item.active').index() + 1;
-
-    if (currentIndex === totalItems) {
-      if (document.fullscreenElement) {
-      document.exitFullscreen()
-    }
-    }
-  });
