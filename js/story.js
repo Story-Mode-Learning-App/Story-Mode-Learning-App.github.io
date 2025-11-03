b2e411707888c166ec3d8f7cecf9fa34e5ac021a
ASG SHOP <maheyan6484@gmail.com>
2025-11-03 13:21:03 +0800

    push

--- js/story.js
+++ js/story.js
@@ -5,6 +5,21 @@ var uid = null;
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     uid = user.uid;
+    if (document.fullscreenEnabled ||
+          document.webkitFullscreenEnabled ||
+          document.mozFullScreenEnabled ||
+          document.msFullscreenEnabled) {
+
+          if (iframe.requestFullscreen) {
+            iframe.requestFullscreen();
+          } else if (iframe.webkitRequestFullscreen) {
+            iframe.webkitRequestFullscreen();
+          } else if (iframe.mozRequestFullScreen) {
+            iframe.mozRequestFullScreen();
+          } else if (iframe.msRequestFullscreen) {
+            iframe.msRequestFullscreen();
+          }
+        }
   }else {
       window.location.replace("../../../../");
   }
