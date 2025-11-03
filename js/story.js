1dfd62298c636a91a899acb7234e7a37a72b5700
ASG SHOP <maheyan6484@gmail.com>
2025-11-03 14:13:08 +0800

    final fix

--- js/story.js
+++ js/story.js
@@ -34,3 +34,15 @@ function fullscreen() {
     }
   }
 }
+
+    $('#carouselFade').on('slid.bs.carousel', function () {
+    const $carousel = $(this);
+    const totalItems = $carousel.find('.carousel-item').length;
+    const currentIndex = $carousel.find('.carousel-item.active').index() + 1;
+
+    if (currentIndex === totalItems) {
+      if (document.fullscreenElement) {
+      document.exitFullscreen()
+    }
+    }
+  });
--- js/trick.js
+++ js/trick.js
@@ -4,15 +4,3 @@ function buttonChange() {
       document.exitFullscreen()
     }
     }
-
-    $('#carouselFade').on('slid.bs.carousel', function () {
-    const $carousel = $(this);
-    const totalItems = $carousel.find('.carousel-item').length;
-    const currentIndex = $carousel.find('.carousel-item.active').index() + 1;
-
-    if (currentIndex === totalItems) {
-      if (document.fullscreenElement) {
-      document.exitFullscreen()
-    }
-    }
-  });
